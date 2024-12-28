9c70b5c03d201777caba479c0acb950563c19e1c
johnaopedro <johnaopedro@gmail.com>
2024-12-28 00:30:45 -0300

    feat:adicionando outro script

--- actions/despesas_por_orgao.py
+++ actions/despesas_por_orgao.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 import requests
 import json
 import os
@@ -29,17 +29,14 @@ ORGAOS_SUPERIORES = {
     "Ministério da Segurança Pública": 29000,
     "Ministério da Saúde (SUS)": 36001,
 }
-
 #Função para buscar os dados da API do Portal da Transparência
 def fetch_data(pagina, orgao_superior, ano):
     url = f"https://api.portaldatransparencia.gov.br/api-de-dados/despesas/por-orgao?ano={ano}&orgaoSuperior={orgao_superior}&pagina={pagina}"
-    
     #Definindo o cabeçalho com a chave da API e o cookie secretos
     headers = {
         'chave-api-dados': API_KEY,
         'Cookie': API_COOKIE
     }
-    
     try:
         #Requisição GET à API
         response = requests.get(url, headers=headers)
@@ -84,14 +81,12 @@ def fetch_and_save_data(ano):
                 if item_serialized not in seen:
                     all_data.append(item)
                     seen.add(item_serialized)
-            
             #Avança para a próxima página
             pagina += 1
         #Se houver dados, salva no arquivo JSON (acumulando com os dados existentes)
         if all_data:
             with open(output_file, "w", encoding="utf-8") as file:
                 json.dump(all_data, file, ensure_ascii=False, indent=4)
-            
             print(f"Dados atualizados com sucesso em {output_file}")
         else:
             print(f"Nenhum dado encontrado para o órgão {orgao}. Arquivo não alterado.")
