aad61c2f20363372d1cc851a507e646237625239
@gabriel.barbaceli <gabriel.barbaceli@gmail.com>
2025-01-26 17:10:18 -0300

    refactor: refatorei a tela de tabelas, implementei verificacoes automaticas de qualidade de software

--- actions/compras.py
+++ actions/compras.py
@@ -1,10 +1,14 @@
-import requests
-import re
 import json
-from datetime import datetime
 import os
+import re
+from datetime import datetime
 from urllib.parse import urlencode
+
+import requests
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
 def capturar_e_filtrar(desde_o_dia):
     base_url = "https://queridodiario.ok.org.br/api/gazettes"
     params = {
@@ -12,64 +16,101 @@ def capturar_e_filtrar(desde_o_dia):
         "published_since": desde_o_dia,
         "querystring": '"cujo objeto é a aquisição do item identificado pelo Código"',
         "excerpt_size": "3000",
-        "number_of_excerpts": "10000","pre_tags": "","post_tags": "",
+        "number_of_excerpts": "10000",
+        "pre_tags": "",
+        "post_tags": "",
         "size": "10000",
-        "sort_by": "ascending_date"}
-    response = requests.get(f'{base_url}?{urlencode(params)}')
+        "sort_by": "ascending_date",
+    }
+    response = requests.get(f"{base_url}?{urlencode(params)}")
     if response.status_code != 200:
         print("Erro:", response.status_code)
         return None
     dados = response.json()
     dados_por_data = {}
-    for gazette in dados['gazettes']:
-        data = gazette['date']
+    for gazette in dados["gazettes"]:
+        data = gazette["date"]
         dados_por_data.setdefault(data, [])
         total_diario = 0
         excerto_relevante_encontrado = False
-        for excerto in gazette['excerpts']:
-            excerto = re.sub(r'(\w)-\n(\w)', r'\1\2', excerto).replace('\n', ' ').strip()
-            empresa_match = re.search(r'empresa\s+([\w\s\-ÇçÉéÁáÍíÓóÚúÃãÕõâêîôûÂÊÎÔÛäëïöüÄËÏÖÜ]+)\s*-\s*CNPJ:\s*(\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2})', excerto, re.IGNORECASE)
-            objeto_match = re.search(r'cujo\s+objeto\s+é\s+a\s+aquisição\s+do\s+item\s+identificado\s+pelo\s+Código\s+SES\s+\d+\s*-\s*([^\n,]+)', excerto, re.IGNORECASE)
-            valor_match = re.search(r'valor\s+global\s+de\s+R\$\s*([\d.,]+)', excerto, re.IGNORECASE)
+        for excerto in gazette["excerpts"]:
+            excerto = (
+                re.sub(r"(\w)-\n(\w)", r"\1\2", excerto)
+                .replace("\n", " ")
+                .strip()
+            )
+            empresa_match = re.search(
+                r"empresa\s+([\w\s\-ÇçÉéÁáÍíÓóÚúÃãÕõâêîôûÂÊÎÔÛäëïöüÄËÏÖÜ]+)\s*-\s*CNPJ:\s*(\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2})",
+                excerto,
+                re.IGNORECASE,
+            )
+            objeto_match = re.search(
+                r"cujo\s+objeto\s+é\s+a\s+aquisição\s+do\s+item\s+identificado\s+pelo\s+Código\s+SES\s+\d+\s*-\s*([^\n,]+)",
+                excerto,
+                re.IGNORECASE,
+            )
+            valor_match = re.search(
+                r"valor\s+global\s+de\s+R\$\s*([\d.,]+)",
+                excerto,
+                re.IGNORECASE,
+            )
             if empresa_match and objeto_match and valor_match:
                 excerto_relevante_encontrado = True
                 empresa, cnpj = empresa_match.groups()
-                objeto = re.sub(r',\s*para\s+atender\s+as\s+necessidades.*', '', objeto_match.group(1), flags=re.IGNORECASE)
+                objeto = re.sub(
+                    r",\s*para\s+atender\s+as\s+necessidades.*",
+                    "",
+                    objeto_match.group(1),
+                    flags=re.IGNORECASE,
+                )
                 valor = valor_match.group(1).strip()
-                valor_limpo = re.sub(r'[^\d,\.]', '', valor)
-                if ',' in valor_limpo:
-                    valor_limpo = valor_limpo.replace('.', '').replace(',', '.')
+                valor_limpo = re.sub(r"[^\d,\.]", "", valor)
+                if "," in valor_limpo:
+                    valor_limpo = valor_limpo.replace(".", "").replace(
+                        ",", "."
+                    )
                 try:
                     valor_float = float(valor_limpo)
                 except ValueError:
                     print(f"Erro ao converter o valor: {valor_limpo}")
                     valor_float = 0
                 total_diario += valor_float
-                dados_por_data[data].append({
-                    "Empresa": empresa.strip(),
-                    "CNPJ": cnpj.strip(),
-                    "Objeto": objeto.strip(),
-                    "Valor": f"R${valor_float:.2f}"})
+                dados_por_data[data].append(
+                    {
+                        "Empresa": empresa.strip(),
+                        "CNPJ": cnpj.strip(),
+                        "Objeto": objeto.strip(),
+                        "Valor": f"R${valor_float:.2f}",
+                    }
+                )
         if excerto_relevante_encontrado:
-            print(f'Data: {data}, Dívidas totais = R${total_diario:.2f}')
+            print(f"Data: {data}, Dívidas totais = R${total_diario:.2f}")
         else:
             dados_por_data.pop(data, None)
     return dados_por_data
+
+
 def salvar_dados():
     json_folder, json_path = "json", os.path.join("json", "compras.json")
     os.makedirs(json_folder, exist_ok=True)
-    desde_o_dia = datetime.now().strftime("%Y-%m-%d") if os.path.exists(json_path) else "2024-01-01"
+    desde_o_dia = (
+        datetime.now().strftime("%Y-%m-%d")
+        if os.path.exists(json_path)
+        else "2024-01-01"
+    )
     new_data = capturar_e_filtrar(desde_o_dia)
     if not new_data:
         print("Nenhum dado novo para adicionar.")
         return
     dados_existentes = {}
     if os.path.exists(json_path):
-        with open(json_path, "r", encoding='utf-8') as file:
+        with open(json_path, "r", encoding="utf-8") as file:
             dados_existentes = json.load(file)
     for data, itens in new_data.items():
         dados_existentes.setdefault(data, []).extend(itens)
-    with open(json_path, "w", encoding='utf-8') as file:
+    with open(json_path, "w", encoding="utf-8") as file:
         json.dump(dados_existentes, file, ensure_ascii=False, indent=4)
     print(f"Dados atualizados salvos em '{json_path}'")
-salvar_dados() #INICIO
+
+
+salvar_dados()  # INICIO
---
+++ codigo/static/scripts/tabelas.js
@@ -0,0 +1,307 @@
+let secaoAtual = "compras";
+let despesasAtuais = [];
+
+const meses = [
+  "Janeiro",
+  "Fevereiro",
+  "Março",
+  "Abril",
+  "Maio",
+  "Junho",
+  "Julho",
+  "Agosto",
+  "Setembro",
+  "Outubro",
+  "Novembro",
+  "Dezembro",
+];
+
+const obterBarraDePesquisa = () => document.getElementById("search-input");
+
+const obterFiltroSelect = () => document.getElementById("filter-select");
+
+const obterValorPadraoDoFiltro = () => document.getElementById("valor-padrao");
+
+function exibirQuantidadeResultados(dados, quantidadeDeResultados) {
+  if (dados.length === 0) {
+    quantidadeDeResultados.textContent = "Nenhum resultado encontrado.";
+    return;
+  }
+  quantidadeDeResultados.textContent = `Exibindo ${dados.length} resultado(s).`;
+}
+
+function renderizarTabela(dados) {
+  const tbody = document.querySelector("#table-container");
+  const quantidadeDeResultados = document.querySelector("#result-count");
+  tbody.innerHTML = "";
+
+  exibirQuantidadeResultados(dados, quantidadeDeResultados);
+
+  if (dados.length === 0) return;
+
+  const tabelaHtml = `
+      <table class="data-table">
+        <thead>
+          ${
+            secaoAtual === "compras"
+              ? `
+          <tr>
+            <th>Empresa</th>
+            <th>CNPJ</th>
+            <th>Objeto</th>
+            <th>Valor</th>
+            <th>Data</th>
+          </tr>`
+              : `
+          <tr>
+            <th>Ano</th>
+            <th>Órgão</th>
+            <th>Código do Órgão</th>
+            <th>Empenhado</th>
+            <th>Liquidado</th>
+            <th>Pago</th>
+          </tr>`
+          }
+        </thead>
+        <tbody>
+          ${
+            secaoAtual === "compras"
+              ? dados
+                  .map((item) => {
+                    return `<tr>
+              <td>${item.empresa}</td>
+              <td>${item.cnpj}</td>
+              <td>${item.objeto}</td>
+              <td>R$ ${item.valor.toFixed(2)}</td>
+              <td>${item.data}</td>
+            </tr>`;
+                  })
+                  .join("")
+              : dados
+                  .map((item) => {
+                    return `<tr>
+                <td>${item.ano}</td>
+                <td>${item.orgao}</td>
+                <td>${item.codigoOrgao}</td>
+                <td>R$ ${item.empenhado.toFixed(2)}</td>
+                <td>R$ ${item.liquidado.toFixed(2)}</td>
+                <td>R$ ${item.pago.toFixed(2)}</td>
+              </tr>`;
+                  })
+                  .join("")
+          }
+        </tbody>
+      </table>
+    `;
+
+  tbody.innerHTML = tabelaHtml;
+}
+
+function montarValorPadraoDoFiltro(valor) {
+  const valorPadrao = obterValorPadraoDoFiltro();
+  valorPadrao.textContent = `Todos os ${valor}`;
+}
+
+function montarOpcoesFiltro(opcoes, filtroSelect) {
+  opcoes.forEach((opcao, index) => {
+    const mesNumero = index < 10 ? `0${index + 1}` : index + 1;
+
+    const opcaoElement = document.createElement("option");
+    opcaoElement.value = secaoAtual === "compras" ? mesNumero : opcao;
+    opcaoElement.textContent = opcao;
+    filtroSelect.appendChild(opcaoElement);
+  });
+}
+
+function montarOpcoesFiltroMes(filtroSelect) {
+  montarValorPadraoDoFiltro("Meses");
+
+  montarOpcoesFiltro(meses, filtroSelect);
+}
+
+function montarOpcoesFiltroAno(filtroSelect) {
+  montarValorPadraoDoFiltro("Anos");
+
+  montarOpcoesFiltro(anos, filtroSelect);
+}
+
+function desmontarOpcoesFiltro(filtroSelect) {
+  const valorPadrao = document.getElementById("valor-padrao");
+  const valorPadraoTexto = valorPadrao ? valorPadrao.textContent : "";
+
+  // Limpa todas as opções, exceto a de valor-padrao
+  filtroSelect.innerHTML = "";
+
+  if (valorPadrao) {
+    const opcaoPadrao = document.createElement("option");
+    opcaoPadrao.id = "valor-padrao";
+    opcaoPadrao.textContent = valorPadraoTexto;
+    opcaoPadrao.value = "";
+    filtroSelect.appendChild(opcaoPadrao);
+  }
+}
+
+function mostrarTabela() {
+  const filtroSelecionado = obterFiltroSelect();
+
+  const containerPesquisa = obterBarraDePesquisa();
+  const secaoTitulo = document.getElementById("secao-selecionada");
+
+  containerPesquisa.value = "";
+
+  if (secaoAtual === "compras") {
+    desmontarOpcoesFiltro(filtroSelecionado);
+    montarOpcoesFiltroMes(filtroSelecionado);
+    secaoTitulo.textContent = "Compras";
+
+    renderizarTabela(gastosCompras);
+  } else if (secaoAtual === "despesas") {
+    desmontarOpcoesFiltro(filtroSelecionado);
+    montarOpcoesFiltroAno(filtroSelecionado);
+
+    renderizarTabela(despesasAtuais);
+  }
+}
+
+function mostrarDespesasPorOrgao(orgao) {
+  const nomeOrgao = orgao.split(" ").slice(1).join(" ");
+  document.getElementById("secao-selecionada").textContent = nomeOrgao;
+  despesasAtuais = despesasPorOrgao[orgao];
+  mostrarTabela();
+}
+
+function PesquisarTabelaCompras() {
+  const pesquisa = obterBarraDePesquisa().value.toLowerCase().trim();
+  const mesSelecionado = obterFiltroSelect().value;
+
+  const dadosFiltrados = gastosCompras.filter((item) => {
+    const mesDaData = item.data.split("/")[1];
+
+    const correspondeAPesquisa =
+      pesquisa === "" ||
+      item.empresa.toLowerCase().includes(pesquisa) ||
+      item.cnpj.toLowerCase().includes(pesquisa) ||
+      item.objeto.toLowerCase().includes(pesquisa) ||
+      item.data.includes(pesquisa) ||
+      item.valor.toString().includes(pesquisa);
+
+    if (!mesSelecionado) return correspondeAPesquisa;
+
+    const correspondeAoMes = mesDaData === mesSelecionado;
+
+    return correspondeAoMes && correspondeAPesquisa;
+  });
+
+  renderizarTabela(dadosFiltrados);
+}
+
+function PesquisarTabelaDespesas() {
+  const pesquisa = obterBarraDePesquisa().value.toLowerCase().trim();
+  const anoSelecionado = obterFiltroSelect().value;
+
+  const dadosFiltrados = despesasAtuais.filter((item) => {
+    const correspondeAPesquisa =
+      pesquisa === "" ||
+      item.ano.toString().includes(pesquisa) ||
+      item.orgao.toLowerCase().includes(pesquisa) ||
+      item.codigoOrgao.toString().includes(pesquisa) ||
+      item.empenhado.toString().includes(pesquisa) ||
+      item.liquidado.toString().includes(pesquisa) ||
+      item.pago.toString().includes(pesquisa);
+
+    if (!anoSelecionado) return correspondeAPesquisa;
+
+    const correspondeAoAno = item.ano.toString() === anoSelecionado;
+
+    return correspondeAoAno && correspondeAPesquisa;
+  });
+  renderizarTabela(dadosFiltrados);
+}
+
+function AtivarDropdown() {
+  document.getElementById("dropdownContent").classList.toggle("show");
+}
+
+// Fecha o dropdown se o usuário clicar fora dele
+window.onclick = function (event) {
+  if (!event.target.matches("#dropdownButton")) {
+    const dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      const abrirDropdown = dropdowns[i];
+      if (abrirDropdown.classList.contains("show")) {
+        abrirDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+document
+  .getElementById("dropdownButton")
+  .addEventListener("click", function (event) {
+    event.stopPropagation();
+    AtivarDropdown();
+  });
+
+obterFiltroSelect().addEventListener("change", function () {
+  if (secaoAtual === "compras") PesquisarTabelaCompras();
+  else PesquisarTabelaDespesas();
+});
+
+document
+  .getElementById("botao-pesquisar")
+  .addEventListener("click", function () {
+    if (secaoAtual === "despesas") PesquisarTabelaDespesas();
+    else PesquisarTabelaCompras();
+  });
+
+mostrarTabela();
+
+function baixarTabelas() {
+  const tabelaAtiva = document.querySelector("#table-container table");
+
+  if (!tabelaAtiva) {
+    alert("Nenhuma tabela disponível para download.");
+    return;
+  }
+
+  const cabecalhos = Array.from(tabelaAtiva.querySelectorAll("thead th")).map(
+    (th) => th.innerText.trim()
+  );
+  const linhas = Array.from(tabelaAtiva.querySelectorAll("tbody tr")).map(
+    (tr) => {
+      return Array.from(tr.querySelectorAll("td")).map((td) =>
+        td.innerText.trim()
+      );
+    }
+  );
+
+  const tabelaData = { cabecalhos, linhas };
+
+  fetch("/baixar-tabelas", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ tabelas: [tabelaData] }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erro ao gerar o PDF");
+      }
+      return response.blob();
+    })
+    .then((blob) => {
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = `${secaoAtual}_filtrada.pdf`;
+      document.body.appendChild(a);
+      a.click();
+      window.URL.revokeObjectURL(url);
+      a.remove();
+    })
+    .catch((error) => {
+      console.error(error);
+      alert("Erro ao fazer o download da tabela.");
+    });
+}
